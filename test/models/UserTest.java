eea2f7b988c35df0ea49336c4822517e181e55f7
Kento Nomiyama <nonoca@klab.ai.kyutech.ac.jp>
2016-07-14 17:28:31 +0900

    検索テストを追加 追加テストのバリエーションを追加

--- test/models/UserTest.java
+++ test/models/UserTest.java
@@ -4,7 +4,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by nonoca on 7/14/16.
@@ -24,12 +25,48 @@ public class UserTest {
         actual.setName("mayok");
         actual.setPasswordDigest("hoge");
         actual.save();
+        assertNotNull(actual.name);
+        assertNotNull(actual.passwordDigest);
     }
 
     @Test
-    public void UserAddTest02() throws Exception {
+    public void UserAddTest02(){
         User actual = new User("mayok", "hoge");
         actual.save();
+        assertNotNull(actual.name);
+        assertNotNull(actual.passwordDigest);
+    }
+
+    @Test
+    public void UserAddTest03(){
+        User actual = new User("mayok", null);
+        actual.save();
+        assertNotNull(actual.name);
+        assertNotNull(actual.passwordDigest);
+    }
+
+    @Test
+    public void UserAddTest04(){
+        User actual = new User(null, "hoge");
+        actual.save();
+        assertNotNull(actual.name);
+        assertNotNull(actual.passwordDigest);
+    }
+
+    @Test
+    public void UserAddTest05(){
+        User actual = new User(null, null);
+        actual.save();
+        assertNotNull(actual.name);
+        assertNotNull(actual.passwordDigest);
+    }
+
+    @Test
+    public void UserSelectTest01(){
+        User actual = new User("mayok", "hoge");
+        actual.save();
+        User expect = User.finder.where().eq("user", "mayok").findUnique();
+        assertEquals(actual.getName(), expect.getName());
     }
 
 
