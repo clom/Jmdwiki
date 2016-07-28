38fe3c1bfacef323447d767609181541c3bf839f
Kento Nomiyama <nonoca@klab.ai.kyutech.ac.jp>
2016-07-29 01:28:29 +0900

    add login view

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -1,11 +1,21 @@
 package controllers;
 
+import models.User;
+import play.data.Form;
 import play.mvc.*;
+import play.data.FormFactory;
 
 import views.html.*;
 
+import javax.inject.Inject;
+
 public class LoginController extends Controller {
+
+  @Inject
+  FormFactory formFactory;
+
   public Result login() {
-    return ok(login.render("login"));
+    Form<User> loginForm = formFactory.form(User.class);
+    return ok(login.render(loginForm, "login"));
   }
 }
