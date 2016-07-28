d12fbaf6b77d9b55c2a54b267f486c75d4c3d646
Kento Nomiyama <nonoca@klab.ai.kyutech.ac.jp>
2016-07-29 02:44:10 +0900

    DB接続無しで login, logout 機能の実装 ログイン状態に応じて header の変更

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.User;
+import play.api.data.FormError;
 import play.data.Form;
 import play.mvc.*;
 import play.data.FormFactory;
@@ -17,5 +18,18 @@ public class LoginController extends Controller {
   public Result login() {
     Form<User> loginForm = formFactory.form(User.class);
     return ok(login.render(loginForm, "login"));
+  }
+
+  public Result auth(){
+    Form<User> loginForm = formFactory.form(User.class).bindFromRequest();
+    if(loginForm.hasErrors()){
+      return badRequest(login.render(loginForm, "login"));
+    }
+    session().clear();
+    session("name", loginForm.get().name);
+    session("login", "true");
+    return redirect(routes.ArticleController.index());
+
+
   }
 }
